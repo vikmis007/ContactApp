3c098ca52c40db6be432bc15d743d6fcd3230304
Vikas Mishra <m.vikas@flipkart.com>
2019-10-19 17:25:45 +0530

    Initial project setup created

---
+++ ContactApp/ContactApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ContactApp
+//
+//  Created by Vikas Mishra on 19/10/19.
+//  Copyright © 2019 Vikas Mishra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
