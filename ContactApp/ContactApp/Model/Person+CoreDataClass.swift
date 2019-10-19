a2df4940a6ec58e4331d1e22092332721a8f0990
Vikas Mishra <m.vikas@flipkart.com>
2019-10-19 19:43:10 +0530

    Implemented contact detail page

---
+++ ContactApp/ContactApp/Model/Person+CoreDataClass.swift
@@ -0,0 +1,57 @@
+//
+//  Person+CoreDataClass.swift
+//  ContactApp
+//
+//  Created by Vikas Mishra on 19/10/19.
+//  Copyright © 2019 Vikas Mishra. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+extension CodingUserInfoKey {
+    static let context = CodingUserInfoKey(rawValue: "context")
+}
+
+@objc(Person)
+public class Person: NSManagedObject, Decodable {
+    /// Init method to decode into model object
+    required convenience public init(from decoder: Decoder) throws {
+        guard let managedContext = decoder.userInfo[CodingUserInfoKey.context!] as? NSManagedObjectContext, let entity = NSEntityDescription.entity(forEntityName: "Person", in: managedContext) else {
+            return
+        }
+        self.init(entity: entity, insertInto: managedContext)
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        do {
+            id = try container.decodeIfPresent(Int32.self, forKey: .id)!
+            first_name = try container.decodeIfPresent(String.self, forKey: .first_name)
+            last_name = try container.decodeIfPresent(String.self, forKey: .last_name)
+            profile_pic = try container.decodeIfPresent(String.self, forKey: .profile_pic)
+            url = try container.decodeIfPresent(String.self, forKey: .url)
+            favorite = try container.decodeIfPresent(Bool.self, forKey: .favorite) ?? false
+        } catch {
+
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
+        try container.encode(first_name, forKey: .first_name)
+        try container.encode(last_name, forKey: .last_name)
+        try container.encode(profile_pic, forKey: .profile_pic)
+        try container.encode(favorite, forKey: .favorite)
+        try container.encode(url, forKey: .url)
+
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case first_name
+        case last_name
+        case profile_pic
+        case favorite
+        case url
+    }
+}
---
+++ ContactApp/ContactApp/Model/Person+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Person+CoreDataProperties.swift
+//  ContactApp
+//
+//  Created by Vikas Mishra on 19/10/19.
+//  Copyright © 2019 Vikas Mishra. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Person {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Person> {
+        return NSFetchRequest<Person>(entityName: "Person")
+    }
+
+    @NSManaged public var id: Int32
+    @NSManaged public var first_name: String?
+    @NSManaged public var last_name: String?
+    @NSManaged public var profile_pic: String?
+    @NSManaged public var favorite: Bool
+    @NSManaged public var url: String?
+
+}
